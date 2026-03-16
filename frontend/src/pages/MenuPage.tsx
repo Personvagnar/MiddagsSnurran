5cc84db84be10f82846e4eb2b5b1863a27e42e30
John Skoog <john.skoog.92@gmail.com>
2026-03-16 10:37:59 +0100

    added a addItem pop-up modal with button.

--- frontend/src/pages/MenuPage.tsx
+++ frontend/src/pages/MenuPage.tsx
@@ -3,9 +3,12 @@ import './pages.css';
 import { useEffect, useState } from 'react';
 import { getItems } from '../api/items';
 import type { Item } from '../types/types';
+import AddEditModal from '../components/AddEditModal/AddEditModal';
+import { FaPlus } from 'react-icons/fa';
 
 function MenuPage() {
     const [items, setItems] = useState<Item[]>([]);
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
     useEffect(() => {
         getItems().then(setItems).catch(err => console.error(err))
@@ -25,6 +28,14 @@ function MenuPage() {
                 ))
             )}
         </section>
+        <aside className="addBtn">
+            <button aria-label='addBtn' onClick={() => {setIsAddModalOpen(true)}}>
+                <FaPlus size={30} color='blue'/>
+            </button>
+        </aside>
+        {isAddModalOpen && (
+            <AddEditModal closeModal={() => setIsAddModalOpen(false)} />
+        )}
     </main>
   )
 }
