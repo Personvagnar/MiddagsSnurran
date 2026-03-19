65abcd920bb80ffdfad64025f0c53e02bb332a2c
John Skoog <john.skoog.92@gmail.com>
2026-03-19 12:32:59 +0100

    removed PORT for render

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import itemsRouter from './routes/items.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
