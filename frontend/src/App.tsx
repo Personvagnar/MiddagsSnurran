ba090b6f5d6a0651c35016af1f52e81f874ea155
John Skoog <john.skoog.92@gmail.com>
2026-03-19 12:14:22 +0100

    Created slumparenpage with hook and utils for randomizeItem. Added some animations too.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import HomePage from './pages/HomePage'
 import MenuPage from './pages/MenuPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SlumparenPage from './pages/SlumparenPage'
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/menu' element={<MenuPage/>}/>
+        <Route path='/slumparen' element={<SlumparenPage/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/SlumparenPage.tsx
@@ -0,0 +1,46 @@
+import { FaCalendarAlt, FaRandom } from 'react-icons/fa';
+import { getItems } from '../api/api';
+import ButtonMain from '../components/ButtonMain/ButtonMain';
+import MenuItem from '../components/MenuItem/MenuItem';
+import type { Item } from '../types/types';
+import './pages.css';
+import { useState, useEffect } from 'react';
+import { useRandomItem } from '../hooks/useRandomItem';
+
+function SlumparenPage() {
+    const [items, setItems] = useState<Item[]>([]);
+    const { randomItem, isAnimating, randomize } = useRandomItem(items);
+
+    useEffect(() => {
+        getItems().then(setItems).catch(err => console.error(err))
+    }, [])
+
+    useEffect(() => {
+        if (items.length > 0) {
+            randomize();
+        }
+    }, [items])
+
+  return (
+    <main>
+        <section className='slumparen'>
+            {isAnimating && <figure className='main-img main-img-slumparen'><img src="/Ellipse 1.png" alt="Main decoartive" /></figure>}
+
+            {!isAnimating && randomItem && (
+                <>
+                <section className='slumparen-content'>
+                <ButtonMain text={randomItem.name} onClick={() => {}} rightIcon={<FaCalendarAlt/>} />
+                <MenuItem item={randomItem}/>
+                </section>
+                <footer>
+                <ButtonMain text='Slumpa igen' rightIcon={<FaRandom />} onClick={randomize}/>
+                </footer>
+                </>
+            )}
+        </section>
+      
+    </main>
+  )
+}
+
+export default SlumparenPage
