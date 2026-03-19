5a892dbdfee5949706056ab3baa2b79975e87695
John Skoog <john.skoog.92@gmail.com>
2026-03-19 14:02:19 +0100

    fixed CORS in backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type"]
+}));
+
+app.options("*", cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 app.use('/api/items', itemsRouter);
