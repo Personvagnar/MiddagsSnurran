050c03f1f8be9392116e9e2869c3d8192fdb6c6c
John Skoog <john.skoog.92@gmail.com>
2026-03-26 12:09:27 +0100

    fixed bugs and re-colored events based on enum protein.

--- frontend/src/pages/SlumparenPage.tsx
+++ frontend/src/pages/SlumparenPage.tsx
@@ -6,10 +6,15 @@ import type { Item } from '../types/types';
 import './pages.css';
 import { useState, useEffect } from 'react';
 import { useRandomItem } from '../hooks/useRandomItem';
+import { useNavigate } from 'react-router-dom';
+import ConfirmationModal from '../components/ConfirmationModal/ConfirmationModal';
+
 
 function SlumparenPage() {
+    const navigate = useNavigate();
     const [items, setItems] = useState<Item[]>([]);
     const { randomItem, isAnimating, randomize } = useRandomItem(items);
+    const [confirmModal, setConfirmModal] = useState(false);
 
     useEffect(() => {
         getItems().then(setItems).catch(err => console.error(err))
@@ -20,6 +25,11 @@ function SlumparenPage() {
             randomize();
         }
     }, [items])
+    
+    function handleConfirmClick() {
+        if (!randomItem) return;
+        navigate('/calendar', {state: { selectedItem: randomItem }});
+    }
 
   return (
     <main>
@@ -29,13 +39,16 @@ function SlumparenPage() {
             {!isAnimating && randomItem && (
                 <>
                 <section className='slumparen-content'>
-                <ButtonMain text={randomItem.name} onClick={() => {}} rightIcon={<FaCalendarAlt/>} />
+                <ButtonMain text={randomItem.name} onClick={() => {}} onRightIconClick={() => setConfirmModal(true)} rightIcon={<FaCalendarAlt/>} />
                 <MenuItem item={randomItem} isOpen={true}/>
                 </section>
                 <footer>
                 <ButtonMain text='Slumpa igen' rightIcon={<FaRandom />} onClick={randomize}/>
                 </footer>
                 </>
+            )}
+            {confirmModal && (
+                <ConfirmationModal text='Lägg till i dagens datum?' onCancel={() => setConfirmModal(false)} onConfirm={handleConfirmClick} />
             )}
         </section>
       
